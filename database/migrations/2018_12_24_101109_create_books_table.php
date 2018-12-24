424ad4f369dcd55b88078cc33ac21d1e10f67d90
verusdeveloper <verusdeveloper@gmail.com>
2018-12-24 13:31:48 +0300

    Создание структуры БД

---
+++ database/migrations/2018_12_24_101109_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',100);
+            $table->string('author',150);
+            $table->integer('genere');
+            $table->string('cover');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
